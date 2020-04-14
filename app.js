f004348c7516d4e0f2462e6b471b4d67a1752376
Matt Carter <m@ttcarter.com>
2020-04-14 12:52:17 +1000

    Mount process now runs in background by default

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ var commander = require('commander');
 var debug = require('debug')('fuse.mongo');
 require('commander-extras');
 var FuseMongoose = require('./index');
+var spawn = require('child_process').spawn;
 
 var program = commander
 	.version(require('./package.json').version)
 	.name('fuse.mongo')
 	.usage('<URI> <mount-point>')
 	.option('-o, --opt <key=val...>', 'CSV of dotted notation config options to populate', (v, t) => t.concat(v.split(/\s*,\s*/)), [])
+	.option('-f, --foreground', 'Run process in foreground (automatically implies --debug)')
 	.option('--debug', 'Enable debug mode, automatically detected with shell variable DEBUG')
 	.option('-v, --verbose', 'Be verbose, specify multiple times for more verbosity', (t, v) => v + 1, 0)
 	.note('Multiple config options can be provided via `-o opt1=val1,opt2=val2`')
@@ -17,6 +19,15 @@ var program = commander
 	.note('Some "lazy" Connection URIs automatically corrected into full MongoURI strings: "database" -> "mongodb://localhost/database", "host/database" -> "mongodb://localhost/database"')
 	.example('fuse.mongo localhost/test /media/testdb', 'Mount localhost "test" database as /media/testdb')
 	.parse(process.argv)
+
+if (!program.foreground) {
+	debug('Forking to background');
+	spawn(process.argv.shift(), [...process.argv, '--foreground'], {
+		stdio: 'inherit',
+		detached: true,
+	});
+	process.exit(0);
+}
 
 /**
 * Storage for this sessions data
